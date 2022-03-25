bd171ac24c85565ccb54c4dfb2a37c19a79e18ef
razibhossain20 <razib.hossain@bs-23.net>
2022-03-25 15:18:11 +0600

    Main content min height updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -328,3 +328,23 @@ $window.on("load", function (e) {
 });
 
 
+//------------- Content Area Min Heihgt -----------
+let calculateAndApplyMinHeight = function () {
+    let topbarHeight = $('.top-ribbon-blue').outerHeight();
+    let navbarHeight = $('.navbar-wrapper').outerHeight();
+    let headerHeight = $('.navbar-wrapper').length > 0 ? topbarHeight+navbarHeight: 0;
+    let footerHeight = $('footer').outerHeight();
+    let windowHeight = $(window).outerHeight();
+    let contentAreaHeight = windowHeight - (footerHeight + headerHeight);
+    $('main').css('min-height', contentAreaHeight > 0 ? contentAreaHeight : 0);
+  };
+  
+  $(document).ready(function () {
+    if(self === top){
+      setTimeout(function () {
+        calculateAndApplyMinHeight();
+      }, 0);
+    }
+  });
+
+
